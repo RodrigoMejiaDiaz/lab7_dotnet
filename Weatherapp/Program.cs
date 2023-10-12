7e6cbc02d0c3e8369fb71b0d7dad529acb3e11bd
Rodrigo Mejia <rodrigoemdiaz@hotmail.com>
2023-10-12 15:14:00 -0500

    3

--- Weatherapp/Program.cs
+++ Weatherapp/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://ip172-18-0-57-ckk40v0gftqg008njr1g-5200.direct.labs.play-with-docker.com/weatherforecast") });
 
 await builder.Build().RunAsync();
